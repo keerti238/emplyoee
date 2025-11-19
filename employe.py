3c0ecb95f8f0f9c62d239d9fc32a8de2b3782eb2
Keerti.Bannimatti <01fe24bca238@kletech.ac.in>
2025-11-19 18:00:49 +0530

    modified

--- employe.py
+++ employe.py
@@ -4,7 +4,7 @@ def get_employee_details(name, emp_id, department, salary):
       f"Employee Name: {name}\n"
       f"Employee ID: {emp_id}\n"
       f"Department: {department}\n"
-      f"Salary: ${salary:,.2f}"  
+      f"Salary: {salary:}"  
   )
 if __name__ == "__main__":
     print(get_employee_details("Smith", "EMP001", "Marketing", 65000))
